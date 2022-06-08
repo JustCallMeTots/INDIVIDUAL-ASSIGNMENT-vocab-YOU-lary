6730ce35a8089bd6f5b0e4e894dc55d30bacd9f4
Micah Stotsky <Leowyatt09@gmail.com>
2022-06-07 21:36:51 -0500

    events and pages

--- src/scripts/helpers/clearDom.js
+++ src/scripts/helpers/clearDom.js
@@ -1,3 +1,8 @@
-const clearDom = () => {};
+const clearDom = () => {
+  document.querySelector('#navBar').innerHTML = '';
+  document.querySelector('#new-word-btn').innerHTML = '';
+  document.querySelector('#vocab-display').innerHTML = '';
+  document.querySelector('#form-shit').innerHTML = '';
+};
 
-export default clearDom
+export default clearDom;
--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -1,1 +1,19 @@
-const startApp = () => {};
+import domBuilder from '../components/domBuilder';
+import logoutButton from '../components/buttons/logoutButton';
+import navBar from '../components/navBar';
+import { showWords } from '../components/pages/words';
+import { getWords } from '../../api/wordData';
+import domEvents from '../events/domEvents';
+import formEvents from '../events/formEvents';
+
+const startApp = () => {
+  domBuilder();
+  domEvents();
+  formEvents();
+  navBar();
+  logoutButton();
+
+  getWords().then((wordsArray) => showWords(wordsArray));
+};
+
+export default startApp;
